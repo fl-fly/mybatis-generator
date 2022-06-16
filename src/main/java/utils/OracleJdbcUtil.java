aedcdad752ffac5e390a143873342e56c06d0d4a
fenglei37804 <fenglei37804@hundsun.com>
2022-06-16 14:23:24 +0800

    mybatis插件升级,解决mapper.xml代码重复生成的问题

--- src/main/java/utils/OracleJdbcUtil.java
+++ src/main/java/utils/OracleJdbcUtil.java
@@ -1,4 +1,4 @@
-package mybatisgenerator;
+package utils;
 
 import java.sql.*;
 
@@ -14,9 +14,9 @@ public class OracleJdbcUtil {
 
         //加载驱动和地址，及地址连接
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        String dbURL = "jdbc:oracle:thin:@10.20.38.241:1521/orcl";
+        String dbURL = "jdbc:oracle:thin:@1x.xx.xx.xxx:1521/orcl";
         //我使用的是oracle12c，这里是没有scott用户的，我自己新建的用户c##scott
-        conn = DriverManager.getConnection(dbURL, "HSRISK5", "HSRISK5");
+        conn = DriverManager.getConnection(dbURL, "xxxxx", "xxxx");
 
         System.out.println(conn);
     }
